5021953fd53eb78798d382a418c22a9af5ce4b10
Muskan <muskaan2lamba@gmail.com>
2020-08-12 01:33:06 +0530

    Updates

---
+++ precache-manifest.006b0b890c84b9578483ed55ebfa6655.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b698774cbc3613310eebeb18479e5c7",
+    "url": "/react-bootstrap/index.html"
+  },
+  {
+    "revision": "8d7f610ab9585284d285",
+    "url": "/react-bootstrap/static/css/2.d2f44748.chunk.css"
+  },
+  {
+    "revision": "43a7ebbef8834354ecaf",
+    "url": "/react-bootstrap/static/css/main.2e71118b.chunk.css"
+  },
+  {
+    "revision": "8d7f610ab9585284d285",
+    "url": "/react-bootstrap/static/js/2.1b3901de.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-bootstrap/static/js/2.1b3901de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a7ebbef8834354ecaf",
+    "url": "/react-bootstrap/static/js/main.9126f6db.chunk.js"
+  },
+  {
+    "revision": "7f7bcd67d8e9998c55c8",
+    "url": "/react-bootstrap/static/js/runtime-main.de0390de.js"
+  }
+]);
